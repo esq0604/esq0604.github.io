00d42e115884fdc80687056ec00d99f00bf22c9b
esq0604 <78311744+esq0604@users.noreply.github.com>
2021-06-24 20:06:38 +0900

    Create while02.c

---
+++ c/while02.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  /*
+  int i=18
+  do-while
+  do{
+    printf("%d\n",i);
+    i++
+    }while(i<=10); do안의 내용은 일단 실행을한번함. 그뒤 반복 결정.
+  무한반복
+ whlihe(ture){
+   printf("%d",i);
+   i++;
+   }
+    int n
+    do{
+    scanf("%d",&n;);
+    }whlie(n!=0);
+    printf("~");  -입력값이 0일때까지 무한반복. -n이 정의가 안되어있는데 루프를 할때 씀.
+}
