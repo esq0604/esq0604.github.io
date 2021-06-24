9d300471c69667b67f77a236c3a5e8976dd70e78
esq0604 <78311744+esq0604@users.noreply.github.com>
2021-06-24 20:42:26 +0900

    Create for03.c

---
+++ c/for03.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  /*
+  int n;
+  scanf("%d",&n);
+  
+  for(int i=0,t=1; i<=n; i++, t*=2) -두개의 이상의  변수도 가능함.
+  {
+    prinf("2^%d=%d\n",i,t);
+  }
+  */
+  
+  /*
+  break:반복문 한개를 빠져나옴 , continue , 중첩for문
+  for(int i=0; i<n; i++){
+  for(int j=1; j<i; j++{
+  printf("*");
+  }
+  printf("\n");
+}
